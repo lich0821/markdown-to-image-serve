1c4a10c01d69a46c70f3747f37378db62955d205
Changhua <lichanghua0821@gmail.com>
2025-04-11 07:31:42 +0800

    feat: use Beijing time

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -18,17 +18,11 @@ const Textarea: React.FC<TextareaHTMLAttributes<HTMLTextAreaElement>> = ({ onCha
     );
 };
 
-const defaultMd = `# AI晨报 - 4月29日  
-
-![image](https://imageio.forbes.com/specials-images/imageserve/64b5825a5b9b4d3225e9bd15/artificial-intelligence--ai/960x0.jpg?format=jpg&width=1440)  
-
-1. **MetaElephant公司发布多模态大模型XVERSE-V**：支持任意长宽比的图像输入，在多项权威评测中表现优异，并已开源。  
-2. **通义千问团队开源百亿参数模型Qwen1.5-110B**：采用Transformer解码器架构，支持多种语言，并具备高效的注意力机制。  
-3. **生数科技与清华大学发布视频大模型Vidu**：采用扩散模型（Diffusion）与Transformer融合架构，一键生成高清视频，国际领先。  
-4. **Mutable AI推出Auto Wiki v2**：可自动将代码转换为类似维基百科的文档，解决代码文档化问题。  
-5. **谷歌在美国新建数据中心**：计划投资30亿美元，在印第安纳州建设数据中心园区，扩建弗吉尼亚州设施，并启动人工智能机会基金。  
-6. **中国信息通信研究院发布汽车大模型标准**：旨在规范和促进汽车行业的智能化发展。  
-7. **Kimi Chat移动端应用更新**：版本1.2.1全面改版用户界面，引入全新浅色模式，提供更舒适直观的体验。  `;
+const defaultMd = ``;
+const now = new Date();
+const timezoneOffset = now.getTimezoneOffset() * 60000; // 获取当前时区与UTC的时间差（以毫秒为单位）
+const localDate = new Date(now.getTime() - timezoneOffset);
+const dateString = localDate.toISOString().slice(0, 10);
 
 export default function Editor() {
     const [mdString, setMdString] = useState(defaultMd);
@@ -69,7 +63,7 @@ export default function Editor() {
                             ref={markdownRef}
                         >
                             <Md2PosterHeader className="flex justify-center items-center px-4 font-medium text-lg">
-                                <span>{new Date().toISOString().slice(0, 10)} 【比特金矿】精选线报</span>
+                                <span>{dateString} 【比特金矿】精选线报</span>
                             </Md2PosterHeader>
                             <Md2PosterContent>{mdString}</Md2PosterContent>
                             <Md2PosterFooter className="text-center">
